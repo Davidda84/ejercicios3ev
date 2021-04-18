bd2ab75639466c840a9331d0b1ec7a106f797f62
David Díaz Álvarez <davidda83@educastur.es>
2021-04-18 18:29:19 +0200

    Ejercicio 1 de la unidad 8 (colecciones) resuelto

--- src/unida8/colecciones/Ejercicio1.java
+++ src/unida8/colecciones/Ejercicio1.java
@@ -5,10 +5,6 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Ejercicio1 {
-	/*Programa que lee de la entrada estándar una secuencia de nombres y los guarda en una colección de forma
-	que ésta no contenga nombres repetidos. La primera línea de entrada contiene la cantidad de nombres. El
-	resto de las líneas contienen los nombres a razón de uno por línea. El programa finalizará mostrando los
-	nombres guardados en la colección en el mismo orden en que se insertaron.*/
 	
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
