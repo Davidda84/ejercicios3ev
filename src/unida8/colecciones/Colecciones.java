53c57683c7fe149898150e17e801068b7af990e8
David Díaz Álvarez <davidda83@educastur.es>
2021-05-31 20:58:38 +0200

    “Ejercicio 1 de la unidad 8 (colecciones 2) resuelto

---
+++ src/unida8/colecciones/Colecciones.java
@@ -0,0 +1,32 @@
+package unida8.colecciones;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Colecciones {
+
+	public static void main(String[] args) {
+		
+		//Test1
+		Set<String> set = new HashSet<String>();
+		set.add("uno");
+		set.add("otro");
+		set.add("el");
+		set.add("dos");
+		set.add("un");
+		eliminarLasDeLongitudPar(set);
+
+	}
+
+	private static void eliminarLasDeLongitudPar(Set<String> conjunto) {
+		/*
+		 * Método llamado eliminarLasDeLongitudPar que acepte un conjunto de cadenas y
+		 * elimine del mismo todas aquellas cadenas de una longitud par. No se permite
+		 * el uso de ninguna otra estructura de datos auxiliar.
+		 */
+		conjunto.removeIf(n -> (n.length() % 2 == 0));
+		System.out.println(conjunto);
+	}
+
+}
